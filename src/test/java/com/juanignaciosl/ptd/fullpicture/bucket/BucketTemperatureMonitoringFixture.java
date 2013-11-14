f51df9e95f563c519c2eb5ace52494d61a4fe11e
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2013-11-14 19:54:26 +0100

    Acceptance specification

---
+++ src/test/java/com/juanignaciosl/ptd/fullpicture/bucket/BucketTemperatureMonitoringFixture.java
@@ -0,0 +1,14 @@
+package com.juanignaciosl.ptd.fullpicture.bucket;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class BucketTemperatureMonitoringFixture {
+	
+	public String bucketTemperature(String inputTemperature) {
+		// TODO: WIP
+		return null;
+	}
+
+}
